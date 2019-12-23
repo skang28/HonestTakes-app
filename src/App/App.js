17d0a6f241a2946df0ba14136a846381dd1a4503
Steven Kang <skang28@gmail.com>
2019-12-23 16:02:51 -0500

    initial files for app

---
+++ src/App/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  // set up state. consists of posts
+
+
+  // set up componentDidMount to get all posts from server
+
+  // add posts function
+
+  // delete posts function
+  render() {
+
+  }
+
+
+}
+
+export default App;
